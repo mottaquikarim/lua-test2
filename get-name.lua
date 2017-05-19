62da80d006d047574ac439a1d129b2893964a4f0
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2017-05-18 22:59:04 -0400

    [🎊 commited with luau 🎊] testing asdfasdfa

--- get-name.lua
+++ get-name.lua
@@ -1,7 +1,13 @@
 local module =  {}
 
-function module.greet(name)
-    return "Hello "..name    
+-- one line test tho...
+-- hello faizal
+
+-- define function forEach
+function module.greet(arr, cb) 
+  for i, item in ipairs(arr) do
+    cb(item, i)
+  end
 end
-    
-return module;
+
+return module
